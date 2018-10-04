4bd4517749753a612c65f61cfc5cad719d4822a1
leeseonghyun21 <leeseonghyun21@gmail.com>
2018-10-05 01:02:48 +0900

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="temp">
+        일정관리 앱
+      </div>
+    );
+  }
+}
+
+export default App;
